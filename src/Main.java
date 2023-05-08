eaf5c15d1eb3555a6c6d0aacee0cb1ef7feee231
杨森喜 <sengxi.yang@outlook.com>
2023-05-08 17:10:36 +0800

    测试提交分支修改是否成功

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("测试代码是否正常");
         System.out.println("文件");
+        System.out.println("测试分支修改是否成功");
     }
 }
